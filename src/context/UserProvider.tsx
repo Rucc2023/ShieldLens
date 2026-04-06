5aa1443f7175a0ec2c664cb3e80d8c8ceb09ff36
Alan-Uni <144484127+Alan-Uni@users.noreply.github.com>
2026-04-06 10:47:41 -0600

    contexto global para almacenar el nombre y email del usuario, además del ID y rol. Esto permite que esta información esté disponible en toda la aplicación sin necesidad de pasar props manualmente a través de los componentes. También se actualiza el proceso de login para guardar esta información adicional en el contexto y en el localStorage, asegurando que esté disponible incluso después de recargar la página.

--- src/context/UserProvider.tsx
+++ src/context/UserProvider.tsx
@@ -4,24 +4,35 @@ import { UserContext } from './UserContext';
 export const UserProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const [userId, setUserId] = useState<string | null>(localStorage.getItem('userId'));
   const [userRole, setUserRole] = useState<string | null>(localStorage.getItem('userRole'));
+  const [userName, setUserName] = useState<string | null>(localStorage.getItem('userName')); // <--- Agregado
+  const [userEmail, setUserEmail] = useState<string | null>(localStorage.getItem('userEmail')); // <--- Agregado
 
-  const loginUser = (id: string, role: string) => {
+  const loginUser = (id: string, role: string, name: string, email: string) => {
     setUserId(id);
     setUserRole(role);
+    setUserName(name)
+    setUserEmail(email)
     localStorage.setItem('userId', id);
     localStorage.setItem('userRole', role);
+    localStorage.setItem('userName', name); // <--- Agregado
+    localStorage.setItem('userEmail', email); // <--- Agregado
   };
 
   const logout = () => {
     setUserId(null);
     setUserRole(null);
+    setUserName(null);
+    setUserEmail(null);
+
     localStorage.removeItem('userId');
     localStorage.removeItem('userRole');
+    localStorage.removeItem('userName'); // <--- Agregado
+    localStorage.removeItem('userEmail'); // <--- Agregado
     localStorage.removeItem('token');
   };
 
   return (
-    <UserContext.Provider value={{ userId, userRole, loginUser, logout }}>
+    <UserContext.Provider value={{ userId, userRole, userName, userEmail, loginUser, logout }}>
       {children}
     </UserContext.Provider>
   );
